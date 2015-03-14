932e3848c4862bb89ac311c8fe7a9c4f9bae18d7
Nestel <gabriel.kerekes.ml@hotmail.com>
2015-03-14 23:12:28 +0100

    movement almost done .. jk not even close

--- snake.c
+++ snake.c
@@ -3,15 +3,12 @@
 
 #define FIELD_SIZE 15
 #define SNAKE_INIT_SIZE 3
-#define SNAKE_CHAR S
-#define SNAKE_HEAD_CHAR H
-#define FODD_CHAR F
 #define SNAKE_MAX_SIZE 200
 
 typedef struct {
 	int x;
 	int y;
-}Positions;
+}Position;
 
 typedef struct {
 	Position pos;
@@ -21,6 +18,16 @@ typedef struct {
 	Position pos;
 }Food;
 
+const char SNAKE_HEAD_CHAR = 'H';
+const char SNAKE_CHAR = 'S';
+const char FOOD_CHAR = 'F';
+
+enum direction { 
+	right = 0,
+	up = 1,
+	left = 2, 
+	down = 3, 
+};
 /*
 	-1 -> SNAKE
 	0 -> NOTHING
@@ -29,22 +36,68 @@ typedef struct {
 int field[FIELD_SIZE][FIELD_SIZE];
 SnakeBodyPart snake[SNAKE_MAX_SIZE];
 int snakeSize = SNAKE_INIT_SIZE;
+enum direction snakeDirection;
 Food food;
 
-void move() {
-
+int isDirectionAllowed(enum direction direction) {
+	if ( direction == left && snakeDirection == right) {
+		return false;
+	}
+	if ( direction == right && snakeDirection == left) {
+		return false;
+	}
+	if ( direction == up && snakeDirection == down) {
+		return false;
+	}
+	if ( direction == down && snakeDirection == up) {
+		return false;
+	}
+	return true;
 }
 
-void checkFoodCol() {
+void snakeMove() {
+	char keyPressed;
 
+	keyPressed = getch();
+
+	switch (keyPressed) {
+		case KEY_UP:
+			if ( isDirectionAllowed(up) ) {
+				snakeDirection = up;
+			}
+		case KEY_DOWN:
+			if ( isDirectionAllowed(down) ) {
+				snakeDirection = down;
+			}
+		case KEY_LEFT:
+			if ( isDirectionAllowed(left) ) {
+				snakeDirection = left;
+			}
+		case KEY_RIGHT:
+			if ( isDirectionAllowed(right) ) {
+				snakeDirection = right;
+			}
+	}
+}
+
+int checkFoodCol() {
+	int i;
+
+	for ( i = 0; i < snakeSize; i++ ) {
+		if ( food.pos.x == snake[i].pos.x && food.pos.y == snake[i].pos.y ) {
+			return true;
+		}
+	}
+	return false;
 }
 
 //generates food at random position
 void generateFood() {
+	int i;
 	do {
 		food.pos.x = rand()%FIELD_SIZE;
 		food.pos.y = rand()%FIELD_SIZE;
-	}while ( food.pos.x == snake.pos.x && food.pos.y == snake.pos.y );
+	}while ( checkFoodCol() );
 }
 
 void checkWallCol() {
@@ -72,28 +125,53 @@ void gameInit() {
 	generateFood();
 }
 
-void printField() {
-	int i, j;
+/*
+	iterates through the field and checks if the snake
+	is at that position, if so, then sets the field value to 0
+*/
+void recreateField() {
+	int posX, posY, k;
 
-	for ( i = 0; i < FIELD_SIZE; i++ ) {
-		for ( j = 0; i < FIELD_SIZE; j++ ) {
-			if ( field[i][j] == 0 ) {
-				printw(' ');
-			}
-			else if ( field[i][j] == -1 ) {
-				printw(SNAKE_CHAR);
-			}
-			else if ( field[i][j] == 1 ) {
-				printw(FOOD_CHAR);
+	for ( posX = 0; posX < FIELD_SIZE; posX++ ) {
+		for ( posY = 0; posY < FIELD_SIZE; posY++ ) {
+			for ( k = 0; k < snakeSize; k++ ) {
+				if ( posX == snake[k].pos.x && posY == snake[k].pos.y ) {
+					field[posX][posY] = -1;
+				}
 			}
 		}
 	}
 }
 
-int gameLoop() {
-	printField();
-	while(1){
+void printField() {
+	int i, j;
+	printw(" ---------------\n");
+	for ( i = 0; i < FIELD_SIZE; i++ ) {
+		printw("|");
+		for ( j = 0; j < FIELD_SIZE; j++ ) {
+			if ( field[i][j] == 0 ) {
+				printw(" ");
+			}
+			else if ( field[i][j] == -1 ) {
+				printw("S");
+			}
+			else if ( field[i][j] == 1 ) {
+				printw("F");
+			}
+		}
+		printw("|\n");
+	}
+	printw(" ---------------\n");
+	refresh();
+}
 
+int gameLoop() {
+	while(1){
+		clear();
+		recreateField();
+		printField();
+		snakeMove();
+		usleep(200000);
 	}
 }
 
@@ -113,7 +191,6 @@ int main() {
 	gameLoop();
 
 	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
 	endwin();			/* End curses mode		  */
 
 	return 0;
